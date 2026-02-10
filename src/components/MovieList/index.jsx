945e9632063cb7efbbfb831d3ecd94130f38c42e
Iulian Roman <iulian.roman@cognizant.com>
2026-02-10 11:31:18 +0200

    implemented MovieList component, modified App.jsx with hardcoded input

---
+++ src/components/MovieList/index.jsx
@@ -0,0 +1,24 @@
+import MovieCard from "../MovieCard";
+import "./MovieList.css";
+
+function MovieList({ movies, watchlist, toggleWatchlist }) {
+  if (movies.length === 0 || !movies)
+    return <div className="movie-list-empty">No movies available.</div>;
+
+  const movieList = movies.map((movie) => {
+    const isWatchlisted = watchlist.includes(movie.id);
+
+    return (
+      <MovieCard
+        key={movie.id}
+        movie={movie}
+        isWatchlisted={isWatchlisted}
+        toggleWatchlist={toggleWatchlist}
+      />
+    );
+  });
+
+  return <div className="movie-list">{movieList}</div>;
+}
+
+export default MovieList;
