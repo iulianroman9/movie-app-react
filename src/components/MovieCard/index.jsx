2b74bb2af8e7fe834963f35c516eac28bdebaaf9
Iulian Roman <iulian.roman@cognizant.com>
2026-02-10 10:42:28 +0200

    implemented movie card component, passing a placeholder object in App component to display it

---
+++ src/components/MovieCard/index.jsx
@@ -0,0 +1,28 @@
+import "./MovieCard.css";
+
+function MovieCard({ movie, isWatchlisted, toggleWatchlist }) {
+  const classList = isWatchlisted
+    ? "movie-watchlist-button active"
+    : "movie-watchlist-button";
+  const buttonText = isWatchlisted ? "In watchlist " : "Add to watchlist";
+
+  return (
+    <div className="movie-card">
+      <div className="movie-image">
+        <img src={`/images/${movie.id}.jpg`} alt="movie-poster" />
+      </div>
+      <div className="movie-info">
+        <h2 className="movie-title">{movie.title}</h2>
+        <div className="movie-meta-info">
+          <p>{`Genre: ${movie.genre}`}</p>
+          <p>{`Rating: ${movie.rating}/10`}</p>
+        </div>
+      </div>
+      <button className={classList} onClick={() => toggleWatchlist(movie.id)}>
+        {buttonText}
+      </button>
+    </div>
+  );
+}
+
+export default MovieCard;
