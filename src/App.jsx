a7c214762e25b859f57c9a67406d073c777d7784
Iulian Roman <iulian.roman@cognizant.com>
2026-02-10 11:52:00 +0200

    modified app component to fetch actual list

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,21 @@
+import { useState, useEffect } from "react";
 import MovieList from "./components/MovieList";
 
 function App() {
-  const movies = [
-    {
-      id: 1,
-      title: "Dark Storm",
-      image: "1.jpg",
-      genre: "drama",
-      rating: "8.3",
-    },
-    {
-      id: 2,
-      title: "Whisper of Fate",
-      image: "2.jpg",
-      genre: "fantasy",
-      rating: "7.7",
-    },
-    {
-      id: 3,
-      title: "Beyond the Edge",
-      image: "3.jpg",
-      genre: "horror",
-      rating: "6.3",
-    },
-    {
-      id: 4,
-      title: "Lost in Shadows",
-      image: "4.jpg",
-      genre: "action",
-      rating: "9.3",
-    },
-    {
-      id: 5,
-      title: "Echoes of Power",
-      image: "5.jpg",
-      genre: "fantasy",
-      rating: "6.9",
-    },
-    {
-      id: 6,
-      title: "Grim Reckoning",
-      image: "6.jpg",
-      genre: "drama",
-      rating: "8.4",
-    },
-    {
-      id: 7,
-      title: "Fury's Flight",
-      image: "7.jpg",
-      genre: "action",
-      rating: "9.8",
-    },
-    {
-      id: 8,
-      title: "Path of the Forgotten",
-      image: "8.jpg",
-      genre: "action",
-      rating: "7.2",
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((arr) => setMovies(arr))
+      .catch((error) => console.error(`Error fetching movies ${error}`));
+  }, []);
 
   return (
-    <div>
-      <MovieList movies={movies} watchlist={[1]} toggleWatchlist={() => {}} />
+    <div className="app">
+      <main>
+        <MovieList movies={movies} watchlist={[1]} toggleWatchlist={() => {}} />
+      </main>
     </div>
   );
 }
