74b2509c6690be423af8f78636acd0142c26dbee
Iulian Roman <iulian.roman@cognizant.com>
2026-02-17 17:21:34 +0200

    return empty array if local storage watchlist item has unexpected structure

--- src/utils/storage.jsx
+++ src/utils/storage.jsx
@@ -6,6 +6,28 @@ export function getWatchlist() {
   if (watchlist) {
     try {
       const parsedList = JSON.parse(watchlist);
+
+      if (!Array.isArray(parsedList)) {
+        throw new Error("watchlist is not an array");
+      }
+
+      if (
+        !parsedList.every(
+          (el) =>
+            typeof el === object &&
+            "id" in el &&
+            typeof el.id === "number" &&
+            "title" in el &&
+            typeof el.title === "string" &&
+            "genre" in el &&
+            typeof el.genre === "string" &&
+            "rating" in el &&
+            typeof el.rating === "number",
+        )
+      ) {
+        throw new Error("watchlist items don't respect expected structure");
+      }
+
       return parsedList;
     } catch (error) {
       console.error(`Error parsing watchlist from local storage: ${error}`);
