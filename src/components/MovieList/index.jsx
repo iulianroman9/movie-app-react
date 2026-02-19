c1c33685b1bab319803d3236f15c903def84c4d6
Iulian Roman <iulian.roman@cognizant.com>
2026-02-19 12:57:29 +0200

    removed active from classname because navlink does it automatically, removed host:port from fetch

--- src/components/MovieList/index.jsx
+++ src/components/MovieList/index.jsx
@@ -11,7 +11,7 @@ function MovieList({ view, filters }) {
   const [watchlist, isWatchlisted, toggleWatchlist] = useWatchlist();
 
   useEffect(() => {
-    fetch("http://localhost:5173/movies.json")
+    fetch("movies.json")
       .then((response) => {
         if (!response.ok) {
           throw new Error(`API call error, status: ${response.status}`);
